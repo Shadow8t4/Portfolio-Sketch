9a45453f1c0e56845861b49ec27e5933d007faf6
Alex Huddleston <shadow8t4@tamu.edu>
2018-01-25 04:59:30 -0600

    Gallery images in place locally and linked appropriately, titles next.

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ $(document).ready(function(){
   $('.test-slider').slick({
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToScroll: 1,
+    dots: true,
+    centerMode: true,
+    centerPadding: '20px',
+    adaptiveHeight: true,
+    adaptiveWidth: true
   });
 });
